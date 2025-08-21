32fbaf83dc2d62304eec70c67a84a8dd1cc91258
onegood07 <one_good07@naver.com>
2025-08-21 20:37:24 +0900

    feat: 경고 아이콘 svg 추가

---
+++ src/assets/icons/WarningIcon.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const WarningIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0zM12 9v4m0 4h.01" />
+  </svg>
+);
+
+export default WarningIcon;
